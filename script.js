e7d4cffd72c481af68f621fde854c2366cfaa80b
theharbyco <harshs7309@gmail.com>
2026-02-22 00:09:24 +0530

    script.js

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function viewCart() {
 }
 
 updateCartCount();
+let cart = [];
+let cartOpen = false;
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  updateCart();
+}
+
+function updateCart() {
+  document.getElementById("cart-count").innerText = cart.length;
+
+  let items = document.getElementById("cart-items");
+  let total = 0;
+  items.innerHTML = "";
+
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.innerText = item.name + " - ₹" + item.price;
+    items.appendChild(li);
+    total += item.price;
+  });
+
+  document.getElementById("cart-total").innerText = total;
+}
+
+function toggleCart() {
+  cartOpen = !cartOpen;
+  document.getElementById("cart").classList.toggle("open");
+}
