a9a847e66c2d75b59868b7057944bbb3f9492f5f
theharbyco <harshs7309@gmail.com>
2026-02-22 00:10:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function addToCart(product, price) {
-  cart.push({product, price});
-  localStorage.setItem("cart", JSON.stringify(cart));
-  updateCartCount();
-  alert(product + " added to cart!");
-}
-
-function updateCartCount() {
-  document.getElementById("cart-count").innerText = cart.length;
-}
-
-function viewCart() {
-  let message = "Your Cart:\n\n";
-  let total = 0;
-
-  cart.forEach(item => {
-    message += item.product + " - ₹" + item.price + "\n";
-    total += item.price;
-  });
-
-  message += "\nTotal: ₹" + total;
-  alert(message);
-}
-
-updateCartCount();
 let cart = [];
 let cartOpen = false;
 
