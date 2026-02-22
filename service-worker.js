429b12ca2a42adc1945e693543689bb3110d52a3
theharbyco <harshs7309@gmail.com>
2026-02-22 19:25:29 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("harby-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
