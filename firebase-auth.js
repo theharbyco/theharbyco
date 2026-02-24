e966379557f797e7fe82276bd81beff279350d81
theharbyco <harshs7309@gmail.com>
2026-02-24 22:54:37 +0530

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,37 +1,15 @@
-// 🔐 Replace with YOUR Firebase keys
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com"
-});
-const auth = firebase.auth();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// MODAL
-function openLogin(){ document.getElementById("loginModal").classList.add("show"); }
-function closeLogin(){ document.getElementById("loginModal").classList.remove("show"); }
+const firebaseConfig = {
+  apiKey: "AIzaSyAe_SMY2YU2xXFP79zGMnbw_zKnzLmvhno",
+  authDomain: "theharbyco.firebaseapp.com",
+  projectId: "theharbyco",
+  storageBucket: "theharbyco.firebasestorage.app",
+  messagingSenderId: "241710496520",
+  appId: "1:241710496520:web:8d8caaf54e30731e9d8abd",
+  measurementId: "G-2SRRTGN77K"
+};
 
-// EMAIL
-function emailSignup(){
-  auth.createUserWithEmailAndPassword(
-    email.value,password.value
-  ).then(()=>alert("Account created"))
-   .catch(e=>alert(e.message));
-}
-function emailLogin(){
-  auth.signInWithEmailAndPassword(
-    email.value,password.value
-  ).then(()=>closeLogin())
-   .catch(e=>alert(e.message));
-}
-
-// GOOGLE
-function googleLogin(){
-  const p=new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(p).then(()=>closeLogin());
-}
-
-// STATE
-auth.onAuthStateChanged(u=>{
-  if(u){ document.getElementById("loginBtn").innerText="Logout";
-    document.getElementById("loginBtn").onclick=()=>auth.signOut();
-  }
-});
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
